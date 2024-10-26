a64a12964baf2f9f3564127f7c9e4036fd09111e
Pritam Samui <77156502+pritamart@users.noreply.github.com>
2024-10-26 20:58:55 +0530

    variable

---
+++ variable.js
@@ -0,0 +1,9 @@
+const fixed = 23;
+let maybeChange = 50; // use
+var chnageValue= 34; // not use bez block and function scope
+
+a = 10
+let b ; //undefine
+
+console.log(fixed,maybeChange,chnageValue)
+console.table([fixed,maybeChange,  chnageValue,a,b])
